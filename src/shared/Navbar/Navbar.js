266b9b985ba20271892113739208930b80f9ea70
MarioVillelaLarraza <mario.v@oblekco.com>
2021-08-20 17:34:13 -0500

    Navbar Responsive

--- src/shared/Navbar/Navbar.js
+++ src/shared/Navbar/Navbar.js
@@ -1,42 +1,42 @@
-import React from 'react';
-import { Link, NavLink } from "react-router-dom";
+import React from "react";
+import {Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark sticky-top">
-      <div className="container-fluid nav-wraper">
-        <Link className="navbar-brand" to="/">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+      <Link className="navbar-brand" to="/">
           U-R-US
         </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            
-            <NavLink 
-              exact 
-              className="nav-link" 
-              activeClassName="active" 
-              to="/"
-            >
-
+            <NavLink exact className="nav-link" activeClassName="active" to="/">
               Registro
-              
             </NavLink>
-            
-            <NavLink 
-              exact 
-              className="nav-link" 
-              activeClassName="active" 
+
+            <NavLink
+              exact
+              className="nav-link"
+              activeClassName="active"
               to="/calendario"
             >
-
               Calendario
-              
             </NavLink>
-            
           </div>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
